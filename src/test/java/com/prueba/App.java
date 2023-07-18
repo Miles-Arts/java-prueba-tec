1ff7c01c86d01110ba7f75b73cbed2e6d2ea2a7d
Milton Figueredo Miles <gratno@gmail.com>
2023-07-18 14:52:43 -0500

    Preparando Job

--- src/test/java/com/prueba/App.java
+++ src/test/java/com/prueba/App.java
@@ -4,14 +4,16 @@ public class App {
 
 
     //Invertir un String
-    private void m1invertirString(String text) {
+    private void m1InvertirString(String text) {
+
+
 
     }
 
     public static void main(String[] args) {
 
         App app = new App();
-        app.m1invertirString("aprender");
+        app.m1InvertirString("aprender");
 
     }
 }
