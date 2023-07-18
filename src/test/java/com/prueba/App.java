3686a353a19259216cbe6eee190bcf44f32acd25
Milton Figueredo Miles <gratno@gmail.com>
2023-07-18 14:59:27 -0500

    Preparando Job  read ultimo al first

--- src/test/java/com/prueba/App.java
+++ src/test/java/com/prueba/App.java
@@ -1,12 +1,22 @@
 package com.prueba;
 
+import java.util.ArrayList;
+
 public class App {
 
 
     //Invertir un String
     private void m1InvertirString(String text) {
+        String[] array = text.split("");
+        StringBuilder newText = new StringBuilder();
 
+        for ( int i = array.length; i > 0; i--) {
 
+            newText.append(array[i - 1]);
+
+        }
+
+        System.out.println(newText);
 
     }
 
