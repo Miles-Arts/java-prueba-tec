e65acd1663c55bd829b323deaa555a84326a67a9
Milton Figueredo Miles <gratno@gmail.com>
2023-07-18 15:20:25 -0500

    Reverse and toString

--- src/test/java/com/prueba/App.java
+++ src/test/java/com/prueba/App.java
@@ -7,7 +7,7 @@ public class App {
 
     //Invertir un String
     private void m1InvertirString(String text) {
-        String[] array = text.split("");
+     /*   String[] array = text.split("");
         StringBuilder newText = new StringBuilder();
 
         for ( int i = array.length; i > 0; i--) {
@@ -16,6 +16,9 @@ public class App {
 
         }
 
+        System.out.println(newText);*/
+
+        String newText = new StringBuilder(text).reverse().toString();
         System.out.println(newText);
 
     }
